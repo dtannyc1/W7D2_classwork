e1a77c0a5be3f8c787586f8411ab5fcbd2ec6000
dtannyc1 <dtannyc1@gmail.com>
2023-04-18 04:08:06 -0400

    Created users and session routes, wrote users_controller

--- MusicApp/app/controllers/users_controller.rb
+++ MusicApp/app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
 
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            # maybe flash some errors here
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find_by(params[:id])
+
+        render :show
+    end
 end
