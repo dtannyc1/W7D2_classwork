23c4812ee047c3ec158917490410aae59a10f7d8
dtannyc1 <dtannyc1@gmail.com>
2023-04-18 02:31:15 -0400

    Finished User methods for session token

--- MusicApp/app/models/user.rb
+++ MusicApp/app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+
+
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64
+            return session_token if User.find_by(session_token: session_token).nil?
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
 end
